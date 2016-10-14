f46637e284b35aca660f73c671115a89263cda08
Milos Havlicek <miloshavlicek@gmail.com>
2016-10-14 20:34:50 +0200

    fixes

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -15,23 +15,25 @@ class RouterFactory {
      */
     public static function createRouter() {
         $router = new RouteList;
-        
-        $router[] = new Route('[<locale=cz cz>/]o-nas', 'About:default');
-        $router[] = new Route('[<locale=en>/]about-us', 'About:default');
-        
-        $router[] = new Route('[<locale=cz cz>/]spolecnosti', 'Company:list');
-        $router[] = new Route('[<locale=en>/]companies', 'Company:list');
-        
-        $router[] = new Route('[<locale=cz cz>/]spolecnosti/<id>', 'Company:detail');
-        $router[] = new Route('[<locale=en>/]companies/<id>', 'Company:detail');
-        
-        $router[] = new Route('[<locale=cz cz>/]kontakty', 'Contacts:default');
-        $router[] = new Route('[<locale=en>/]contacts', 'Contacts:default');
-        
-        $router[] = new Route('[<locale=cz cz>/]kriteria', 'Criteria:default');
-        $router[] = new Route('[<locale=en>/]criteria', 'Criteria:default');
-        
-        $router[] = new Route('[<locale=cz cz|en>/]<presenter>/<action>[/<id>]', 'Homepage:default');
+
+        $ssl = in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) ? Route::SECURED : FALSE;
+
+        $router[] = new Route('[<locale=cz cz>/]o-nas', 'About:default', $ssl);
+        $router[] = new Route('[<locale=en>/]about-us', 'About:default', $ssl);
+
+        $router[] = new Route('[<locale=cz cz>/]spolecnosti', 'Company:list', $ssl);
+        $router[] = new Route('[<locale=en>/]companies', 'Company:list', $ssl);
+
+        $router[] = new Route('[<locale=cz cz>/]spolecnosti/<id>', 'Company:detail', $ssl);
+        $router[] = new Route('[<locale=en>/]companies/<id>', 'Company:detail', $ssl);
+
+        $router[] = new Route('[<locale=cz cz>/]kontakty', 'Contacts:default', $ssl);
+        $router[] = new Route('[<locale=en>/]contacts', 'Contacts:default', $ssl);
+
+        $router[] = new Route('[<locale=cz cz>/]kriteria', 'Criteria:default', $ssl);
+        $router[] = new Route('[<locale=en>/]criteria', 'Criteria:default', $ssl);
+
+        $router[] = new Route('[<locale=cz cz|en>/]<presenter>/<action>[/<id>]', 'Homepage:default', $ssl);
         return $router;
     }
 
